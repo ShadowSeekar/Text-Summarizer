78154af16b758d5f9ec4144a5d60e19c518c4f9b
Harshit <raoharshit2025@gmail.com>
2024-12-02 00:24:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,7 @@ import pandas as pd
 import numpy as np
 import re
 import nltk
+from keras.layers import TextVectorization; import tokenizer
 from keras.preprocessing.text import one_hot, Tokenizer
 from nltk.corpus import stopwords
 from keras.models import load_model

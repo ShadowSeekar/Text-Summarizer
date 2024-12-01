8972a3400f8f4a89280d10b8262f947c3e030b74
Harshit <raoharshit2025@gmail.com>
2024-12-01 18:04:11 +0530

    base4.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,8 +38,8 @@ def llm_pipeline(filepath):
 @st.cache_data
 
 def displayPDF(file):
-    with open(file, "rb") as f:
-        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+    #with open(file, "rb") as f:
+    base64_pdf = base64.b64encode(f.read()).decode('utf-8')
     pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="600" type="application/pdf"></iframe>'
     st.markdown(pdf_display, unsafe_allow_html=True)
 
